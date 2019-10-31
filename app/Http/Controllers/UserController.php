c087c6045e1f2668b060ddf977b5d5a50f5a6353
hourai <h.abcgn.s@gmail.com>
2019-10-31 09:52:15 +0900

    ユーザ名変更のコントローラー追加

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return view('name-change');
+
+    }
+
+    public function edit(Request $request)
+    {
+
+
+        return redirect()->route('home');
+    }
+}
