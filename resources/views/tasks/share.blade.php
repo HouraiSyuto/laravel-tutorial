60c26509296f64378e3ffc0afda7898940d312a0
hourai <h.abcgn.s@gmail.com>
2019-12-14 15:28:30 +0900

    post送信にしなくなったことに伴い、エラーメッセージ処理が不要になったため削除

--- resources/views/tasks/share.blade.php
+++ resources/views/tasks/share.blade.php
@@ -7,37 +7,28 @@
         <nav class="panel panel-default">
           <div class="panel-heading">タスクシェア</div>
           <div class="panel-body">
-          @if($errors->any())
-              <div class="alert alert-danger">
-                @foreach($errors->all() as $message)
-                  <p>{{ $message }}</p>
+            <div class="form-group">
+              <label for="title">タイトル</label>
+              <p>{{ old('title', $task->title) }}</p>
+            </div>
+            <div class="form-group">
+              <label for="status">状態</label>
+              <select name="status" disabled="disabled" id="status" class="form-control">
+                @foreach(\App\Task::STATUS as $key => $val)
+                  <option
+                      value="{{ $key }}"
+                      {{ $key == old('status', $task->status) ? 'selected' : '' }}
+                  >
+                    {{ $val['label'] }}
+                  </option>
                 @endforeach
-              </div>
-            @endif
-              @csrf
-
-              <div class="form-group">
-                <label for="title">タイトル</label>
-                <p>{{ old('title', $task->title) }}</p>
-              </div>
-              <div class="form-group">
-                <label for="status">状態</label>
-                <select name="status" disabled="disabled" id="status" class="form-control">
-                  @foreach(\App\Task::STATUS as $key => $val)
-                    <option
-                        value="{{ $key }}"
-                        {{ $key == old('status', $task->status) ? 'selected' : '' }}
-                    >
-                      {{ $val['label'] }}
-                    </option>
-                  @endforeach
-                </select>
-              </div>
-              <div class="form-group">
-                <label for="due_date">期限</label>
-                <input type="text" class="form-control" name="due_date" disabled="disabled" id="due_date"
-                       value="{{ old('due_date', $task->formatted_due_date) }}" />
-              </div>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="due_date">期限</label>
+              <input type="text" class="form-control" name="due_date" disabled="disabled" id="due_date"
+                      value="{{ old('due_date', $task->formatted_due_date) }}" />
+            </div>
           </div>
         </nav>
       </div>
